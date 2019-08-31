d800e414653ea69dc1e5b51c93c2f17b43a4a1de
Rafael Felix <b1scoitorg@gmail.com>
2019-08-31 01:46:45 -0300

    Initial commit

---
+++ MainWindow.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace anydeskresolver
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Form1";
+        }
+                    // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
+            this.ClientSize = new System.Drawing.Size(390, 170);
+            this.Controls.Add(this.lbCtx2);
+            this.Controls.Add(this.btnResolve);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.txtIPAddr);
+            this.Controls.Add(this.pnTop);
+            this.Controls.Add(this.lbTitle2);
+            this.Controls.Add(this.lbTitle1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.pnGeoIP);
+            this.Font = new System.Drawing.Font("Roboto", 8.25F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainWindow";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Discord Resolver";
+            this.TransparencyKey = System.Drawing.Color.Fuchsia;
+            ((System.ComponentModel.ISupportInitialize)(this.btnClose)).EndInit();
+            this.pnGeoIP.ResumeLayout(false);
+            this.pnGeoIP.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        #endregion
+    }
+}
+
